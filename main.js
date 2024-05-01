628f363f9635b2fc132512e3718ae149eab9c019
Imchris <135647453+ImChrisSal@users.noreply.github.com>
2024-05-02 02:30:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.typed-text', {
+    strings: [
+      'Licensed Financial Advisor', 
+      'Your Partner for life.', 
+    ],
+    typeSpeed: 150,
+    backSpeed: 100,
+    backDelay:1020,
+    loop: true
+  });
+  
